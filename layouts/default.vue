886f749a3fc0e7d1b1005f980a6ec60604f8b1f1
mon_sat <tnk@daijiten.info>
2021-10-31 12:02:36 +0900

    style: add front page

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="p-4 sm:p-8 min-h-screen bg-sky-200 flex">
+    <div class="container p-4 sm:p-8 mx-auto rounded-2xl bg-gray-50 leading-loose text-gray-600">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<style>
+div.container > div > h1,
+div.container > div > h2,
+div.container > div > h3,
+div.container > div > h4,
+div.container > div > h5,
+div.container > div > h6,
+div.container > div > p {
+  @apply py-4 px-6;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const header = ref('Nuxt 3 starter template')
+</script>
+
+<template>
+  <div>
+    <h1 class="rounded-md bg-green-200 text-xl text-green-700 font-bold">
+      {{ header }}
+      with <a href="https://vueuse.org/">VueUse</a> and <a href="with VueUse and Windi CSS" target="_blank">Windi CSS</a>.
+    </h1>
+    <p>
+      Edit <strong>layouts/default.vue</strong> and <strong>windi.config.js</strong>.
+    </p>
+  </div>
+</template>
