4aeb5384174b9e8481622923e92c8aa12991664b
mon_sat <tnk@daijiten.info>
2021-10-31 12:01:59 +0900

    style: add windi.config.js

---
+++ windi.config.js
@@ -0,0 +1,13 @@
+import colors from 'windicss/colors'
+
+export default {
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      ...colors,
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
