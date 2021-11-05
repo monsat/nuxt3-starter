9cd38c40a1c91d05621a9af8f1673eddfb97f851
mon_sat <tnk@daijiten.info>
2021-11-05 17:00:01 +0900

    fix: links are broken

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@ const header = ref('Nuxt 3 starter template')
   <div>
     <h1 class="rounded-md bg-green-200 text-xl text-green-700 font-bold">
       {{ header }}
-      with <a href="https://vueuse.org/">VueUse</a> and <a href="with VueUse and Windi CSS" target="_blank">Windi CSS</a>.
+      with <a href="https://vueuse.org/" target="_blank">VueUse</a> and <a href="https://windicss.org/" target="_blank">Windi CSS</a>.
     </h1>
     <p>
       Edit <strong>layouts/default.vue</strong> and <strong>windi.config.js</strong>.
